9b5dbe448ba62c135a45b58563d15e8f28e6482c
chiCHAchiXD <34370740+chiCHAchiXD@users.noreply.github.com>
2023-03-25 07:28:35 +1200

    Update liskov.java
    Programmer has been made aware of theirs Liskov Substitutional Rule violation and now, solved it in neat (and certainly not complicated) fashion!

--- liskov.java
+++ liskov.java
@@ -1,48 +1,67 @@
 
-package com.osmzhttpserver.logger.messageobservers;
+//good liskov
 
-public interface MessageInterface {
-    public String getText();
 
-    public void setText(String text);
-
-    public String getAuthor();
-
-    public void setAuthor(String author);
-}
 
 
 package com.osmzhttpserver.logger.messageobservers;
 
 import com.osmzhttpserver.generalhelpers;
+public class ObjectifulText{
+    private String text;
+    
+    public ObjectifulText(List<char> text){
+        this.text = generalhelpers.createString(text);
+    }
+    
+    public ObjectifulText(String text){
+        this.text = text;
+    }
+    
+    public this getString(){
+        return this.text;
+    }
+    
+}
+
+public interface MessageInterface {
+    public String getText();
+
+    public void setText(ObjectifulText text);
+
+    public String getAuthor();
+
+    public void setAuthor(ObjectifulText author);
+}
+
+
+
 
 public class Message implements MessageInterface {
-    private String text;
-    private String author;
+    private ObjectifulText text;
+    private ObjectifulText author;
 
     public Message(){}
 
-    public Message(List<char> author, String text){
-        setAuthor(generalhelpers.createString(author));
-        setText(generalhelpers.createCharList(text));
+    public Message(ObjectifulText author, ObjectifulText text){
+        setAuthor(author);
+        setText(text);
     }
 
     public String getText() {
-        return text;
+        return text.getString();
     }
 
-    public void setText(List<char> text) {
+    public void setText(ObjectifulText text) {
         this.text = text;
     }
 
     public String getAuthor() {
-        return author;
+        return author.getString();
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(ObjectifulText author) {
         this.author = author;
     }
 }
-
-
 
